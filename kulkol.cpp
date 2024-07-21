7a104d300b016566b37da3b96647fa8311ad2ee1
ksawerykowalczuk <ksawery.kowalczuk@gmail.com>
2024-07-21 12:29:08 +0200

    final version

--- kulkol.cpp
+++ kulkol.cpp
@@ -2,9 +2,48 @@
 
 using namespace std;
 
+bool pusta(string kolejka)
+{
+    bool czypusta = true;
+    for (int i = 0; i < kolejka.length(); i++)
+    {
+        if (kolejka[i] == 'X')
+        {
+            czypusta = false;
+        }
+    }
+    return czypusta;
+}
+
 int main()
 {
     string kolejka;
     cin >> kolejka;
-    cout << kolejka;
+    int licznik=0;
+
+    while (!pusta(kolejka))
+    {
+        for (int i = 0; i < kolejka.length(); i++)
+        {
+            if (i + 1 == kolejka.length())
+            {
+                kolejka[i] = '.';
+            }
+            else
+            {
+                if (kolejka[i] == 'X')
+                {
+                    if (kolejka[i + 1] == '.')
+                    {
+                        kolejka[i] = '.';
+                        kolejka[i+1] = 'X';
+                        i++;
+                    }
+                }
+            }
+        }
+        licznik++;
+        cout << kolejka << endl;
+    }
+    cout << licznik;
 }
